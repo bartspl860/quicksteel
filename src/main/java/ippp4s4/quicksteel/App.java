a00691db602c9a88d99d695c2144013213c551ab
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2024-04-16 23:22:12 +0200

    Ikonka

--- src/main/java/ippp4s4/quicksteel/App.java
+++ src/main/java/ippp4s4/quicksteel/App.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class App extends Application {
         Scene mainwindow = new Scene(fxmlLoader.load(), 1280, 768);
         stage.setTitle("Quicksteel 24.04");
         stage.setScene(mainwindow);
+        stage.getIcons().add(new Image("file:src/main/resources/ippp4s4/quicksteel/icons/qs-icon.png"));
         stage.show();
     }
     public static void main(String[] args) {
